1b7a577fdf08e40542bec1c5da7a14c7775e9712
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2016-07-12 11:25:59 +0300

    work commit

--- make.sh
+++ make.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-set -x
+REGISTRY=corporate-docker-registry.rgs.cinimex.ru:5000
+NAMESPACE=rgs
+IMAGE_NAME=openshift-spring-cloud-test
+RELEASE=863315
 
-SERVICES=$(find . -name Dockerfile -exec dirname {} \;)
-REGISTRY=corporate-docker-registry.rgs.cinimex.ru:5000/
-REVISION=863315
-
-cnt=$(docker run -d -ti -v $(pwd):/tmp/src --privileged rgs/springboot-s2i bash) && \
-    docker exec -ti $cnt /usr/local/s2i/assemble && \
-for j in $SERVICES ; do
-    docker cp $cnt:/opt/app-root/src/source/build/libs/$j build
-done
-docker kill $cnt
-docker rm -f $cnt
-
-for d in $SERVICES ; do
-    n=$(echo $d | sed 's|./||')
-    docker build -t ${REGISTRY}rgs/$n:$REVISION -f $n/Dockerfile .
-    docker push ${REGISTRY}rgs/$n:$REVISION
-done
+docker tag $IMAGE_NAME:base $REGISTRY/$NAMESPACE/$IMAGE_NAME:$RELEASE
+docker push $REGISTRY/$NAMESPACE/$IMAGE_NAME:$RELEASE
